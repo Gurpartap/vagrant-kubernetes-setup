9c8bf08345644d6b7123aa397c3e1fdc2ea5b9bc
Gurpartap Singh <contact@gurpartap.com>
2014-09-22 18:39:06 +0530

    Ensure that binaries are executable

--- up.sh
+++ up.sh
@@ -7,9 +7,11 @@ set -o pipefail
 
 green='\033[00;32m'
 red='\033[00;31m'
+blue='\033[00;34m'
 
-function color_echo {
-  echo -e -n "${2}${1}"
+function cecho {
+  color="${2:-$blue}"
+  echo -e -n "${color}${1}"
   echo -e '\033[0m'
 }
 
@@ -18,29 +20,34 @@ base_dir=$(cd "${base_dir}" && pwd)
 
 required_files="apiserver controller-manager flanneld kubelet kubecfg proxy scheduler"
 
-echo " ---> Step 1: Check prerequisites."
+cecho " ---> Step 1: Check prerequisites."
 
 for file in ${required_files}
 do
   if [ -f "${base_dir}/bin/${file}" ]; then
-    color_echo "[OK]\tFound ${file}." $green
+    cecho "[OK] Found ${file}." $green
   else
-    color_echo "[ERR]\tCouldn't find ${file}. Please read the usage instructions available in the README.md file." $red
+    cecho "[ERR] Couldn't find ${file}. Please read the usage instructions available in the README.md file." $red
+    exit 1
+  fi
+
+  if ! [[ -x "${base_dir}/bin/${file}" ]]; then
+    cecho "[ERR] File ${file} is not executable. Try \`chmod +x ./bin/${file}\`." $red
     exit 1
   fi
 done
 
-echo " ---> Step 2: Destroy existing cluster."
+cecho " ---> Step 2: Destroy existing cluster."
 vagrant destroy -f
 
-echo " ---> Step 3: Provision vagrant."
+cecho " ---> Step 3: Provision vagrant."
 vagrant up
 
-echo " ---> Step 4: Setup ssh tunnel into master."
+cecho " ---> Step 4: Setup ssh tunnel into master."
 vagrant ssh-config master > ssh.config
 ssh -f -nNT -L 8080:127.0.0.1:8080 -F ssh.config master
 
-echo " ---> Done."
+cecho " ---> Done."
 echo ""
 echo "      Use kubecfg on your host machine to interact with Kubernetes API Server running on master."
 echo ""
